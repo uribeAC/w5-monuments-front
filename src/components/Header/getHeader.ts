db60caff6341fad0e6be2a8c73cfdfaafb931f5b
Alex Uribe <repzyyog@gmail.com>
2025-04-01 09:25:19 +0200

    Create Header component

---
+++ src/components/Header/getHeader.ts
@@ -0,0 +1,10 @@
+const getHeader = (): HTMLElement => {
+  const header = document.createElement("header");
+  header.classList.add("main-header");
+
+  header.innerHTML = "<h1 class='title'>Monuments</h1>";
+
+  return header;
+};
+
+export default getHeader;
