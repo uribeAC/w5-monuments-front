412939e7499261759994330c2ac70831668a0cb3
Alex Uribe <repzyyog@gmail.com>
2025-04-03 21:02:22 +0200

    Add tests to nav menu inside the Header component

--- src/components/Header/getHeader.test.ts
+++ src/components/Header/getHeader.test.ts
@@ -3,10 +3,14 @@ import { expect } from "vitest";
 import { test } from "vitest";
 import getHeader from "./getHeader";
 
+const screen = document.createElement("div");
+beforeEach(() => {
+  screen.innerHTML = "";
+});
+
 describe("Given the Header component", () => {
   describe("When it renders", () => {
     test("Then it should show 'Monuments' inside a heading", () => {
-      const screen = document.createElement("div");
       const expectedAppTitle = "Monuments";
 
       const Header = getHeader();
@@ -16,6 +20,44 @@ describe("Given the Header component", () => {
       const appTitle = screen.querySelector("h1");
       expect(appTitle).not.toBeNull();
       expect(appTitle?.textContent).toBe(expectedAppTitle);
+    });
+
+    test("Then it should show 'Monuments list' as a link", () => {
+      const expectedText = "Monuments list";
+      const expectedPath = "/";
+
+      const Header = getHeader();
+
+      screen.appendChild(Header);
+
+      const linkElements = screen.querySelectorAll("a");
+      const filteredLinkElements = Array.from(linkElements).filter(
+        (linkElement) => linkElement.textContent === expectedText,
+      );
+      const linkElement = filteredLinkElements[0];
+
+      expect(linkElement).not.toBeNull();
+      expect(linkElement.textContent).toBe(expectedText);
+      expect(linkElement.pathname).toBe(expectedPath);
+    });
+
+    test("Then it should show 'Add monument' as a link", () => {
+      const expectedText = "Add monument";
+      const expectedPath = "/add-monument";
+
+      const Header = getHeader();
+
+      screen.appendChild(Header);
+
+      const linkElements = screen.querySelectorAll("a");
+      const filteredLinkElements = Array.from(linkElements).filter(
+        (linkElement) => linkElement.textContent === expectedText,
+      );
+      const linkElement = filteredLinkElements[0];
+
+      expect(linkElement).not.toBeNull();
+      expect(linkElement.textContent).toBe(expectedText);
+      expect(linkElement.pathname).toBe(expectedPath);
     });
   });
 });
