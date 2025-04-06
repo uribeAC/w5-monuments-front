e754047bd79a2272d73354664d9dabab68fb98da
Alex Uribe <repzyyog@gmail.com>
2025-04-06 23:01:22 +0200

    Create the sendData function to send form data to backend

--- add/add.ts
+++ add/add.ts
@@ -1,6 +1,7 @@
 import { getApp } from "./components/App/getApp";
 import "../src/styles.css";
 import "@fontsource-variable/lexend/index.css";
+import { sendData } from "./client/sendData";
 
 const rootElement = document.querySelector(".root");
 
@@ -11,3 +12,14 @@ if (!rootElement) {
 const App = getApp();
 
 rootElement.appendChild(App);
+
+const form = document.querySelector("form") as HTMLFormElement;
+
+if (!form) {
+  throw new Error("Missing form element");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+});
